31f4111239fa595277a564d1692fe80bf3870367
glassnekeep <glassnekeep@yandex.ru>
2021-01-10 09:09:34 +0300

    123321

--- src/A.kt
+++ src/A.kt
@@ -1,11 +1,3 @@
 fun main() {
-    var a = readLine()?.toInt()
-    var b = readLine()?.toInt()
-    var c: Int? = null
-    if(a!=null) {
-        if(b!=null) {
-            c = a*b
-        }
-    }
-    print(c)
+    
 }
